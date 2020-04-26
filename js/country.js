187fdda3a0446080cc4db965d341aa22db8704ce
dekDev <nongnont.170442@gmail.com>
2020-04-26 12:29:25 +0700

    about

--- js/country.js
+++ js/country.js
@@ -24,34 +24,14 @@ $(function(){
             <td>${selectedCountry[i].deaths}</td>
             <td>${selectedCountry[i].recovered}</td>
           </tr>`
-
+         
             
           $("#data").append(row) ;
 
         }
 
     });
-    $(function () {
-        var url = "https://pomber.github.io/covid19/timeseries.json";
-        $.getJSON(url, function (result) {
-            console.log(result);
-            var no = 1;
-            var confirmed = 0;
-            var recovered = 0;
-            var deaths = 0;
-            for (var country in result) {
-                var selectedCountry = result[country];
-                var total = selectedCountry.length;
-                confirmed = confirmed + selectedCountry[total - 1].confirmed;
-                recovered = recovered + selectedCountry[total - 1].recovered;
-                deaths = deaths + selectedCountry[total - 1].deaths;
-            }
-            console.log(confirmed);
-            $("#confirmed").append(confirmed);
-            $("#recovered").append(recovered);
-            $("#deaths").append(deaths);
-
-        });
-    });
 
 })
+
+
--- js/index.js
+++ js/index.js
@@ -20,6 +20,27 @@ $(function () {
         }
 
     });
+    $(function () {
+        var url = "https://pomber.github.io/covid19/timeseries.json";
+        $.getJSON(url, function (result) {
+            console.log(result);
+            var confirmed =0;
+            var recovered =0;
+            var deaths =0;
+            for (var country in result) {
+                var selectedCountry = result[country];
+                var total = selectedCountry.length;
+                confirmed = confirmed + selectedCountry[total - 1].confirmed;
+                recovered = recovered + selectedCountry[total - 1].recovered;
+                deaths = deaths + selectedCountry[total - 1].deaths;
+            }
+            console.log(confirmed,recovered,deaths);
+            $("#confirmed").append(confirmed);
+            $("#recovered").append(recovered);
+            $("#deaths").append(deaths);
+            
+        });
+    });
 
 
 
